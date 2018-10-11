f6f01a6a37c5db0a0513d3451ea69e2fec3f45d9
Matthew Mauermann <mauermann5@outlook.com>
2018-10-11 07:59:04 -0500

    Neighbor programs pt2

---
+++ src/ObjectOrientedProgramming/neighborTest.java
@@ -0,0 +1,15 @@
+package ObjectOrientedProgramming;
+
+public class neighborTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+neighborClass joke=new neighborClass();
+joke.sayJoke();
+neighborClass money=new neighborClass();
+money.sayMoney();
+neighborClass conversation=new neighborClass();
+conversation.sayConversation();
+	}
+
+}
