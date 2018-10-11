8d0f514d862e432ee0c4847bf07d51b62258b033
Matthew Mauermann <mauermann5@outlook.com>
2018-10-11 08:06:19 -0500

    Class test

---
+++ src/ObjectOrientedProgramming/classTest.java
@@ -0,0 +1,11 @@
+package ObjectOrientedProgramming;
+
+public class classTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Greeter bob=new Greeter();
+bob.sayHello();
+	}
+
+}
