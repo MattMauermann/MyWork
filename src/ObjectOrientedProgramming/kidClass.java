0d4741436707755caf576af65374b65a55ed225d
Matthew Mauermann <mauermann5@outlook.com>
2018-10-11 07:59:25 -0500

    Kid Programs

---
+++ src/ObjectOrientedProgramming/kidClass.java
@@ -0,0 +1,21 @@
+package ObjectOrientedProgramming;
+
+public class kidClass {
+
+public kidClass()
+{
+	}
+public void sayMama()
+{
+	System.out.println("Mama");
+}
+public void sayFood()
+{
+	System.out.println("I want food");
+}
+public void sayStink()
+{
+	System.out.println("I stink!");
+}
+
+}
---
+++ src/ObjectOrientedProgramming/kidTest.java
@@ -0,0 +1,15 @@
+package ObjectOrientedProgramming;
+
+public class kidTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+kidClass mama=new kidClass();
+mama.sayMama();
+kidClass food=new kidClass();
+food.sayFood();
+kidClass stink=new kidClass();
+stink.sayStink();
+	}
+
+}
