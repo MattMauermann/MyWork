bdee0d000c2caf2ee1bcba9a88486757c1d3f61f
Matthew Mauermann <mauermann5@outlook.com>
2018-10-11 08:36:32 -0500

    This is the numbers program

---
+++ src/ObjectOrientedProgramming/Numbers.java
@@ -0,0 +1,16 @@
+package ObjectOrientedProgramming;
+
+public class Numbers {
+	//instance fields
+	
+	//constructors
+public Numbers()
+{
+}
+public void sayNumber(int num)
+{
+	System.out.println("You sent " + num + " to the method");
+}
+
+//methods
+}
---
+++ src/ObjectOrientedProgramming/numbersTest.java
@@ -0,0 +1,11 @@
+package ObjectOrientedProgramming;
+
+public class numbersTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Numbers bob=new Numbers();
+bob.sayNumber(3);
+	}
+
+}
