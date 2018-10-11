178dd234713c5312f4e704596266813cedee8a10
Matthew Mauermann <mauermann5@outlook.com>
2018-10-11 08:05:21 -0500

    Bob and joe programs

---
+++ src/ObjectOrientedProgramming/bobandJoe.java
@@ -0,0 +1,17 @@
+package ObjectOrientedProgramming;
+
+public class bobandJoe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Greeter bob=new Greeter();
+Friend John= new Friend();
+bob.sayHello();
+bob.sayHello();
+bob.sayHello();
+John.sayMessage();
+bob.sayHello();
+
+	}
+
+}
