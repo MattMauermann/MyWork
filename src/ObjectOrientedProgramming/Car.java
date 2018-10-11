1885ebba3f534aa1fb84ec5a0849831aa59952e3
Matthew Mauermann <mauermann5@outlook.com>
2018-10-11 07:58:33 -0500

    Updated car programs *car real isnt relevant

---
+++ src/ObjectOrientedProgramming/Car.java
@@ -0,0 +1,22 @@
+package ObjectOrientedProgramming;
+
+public class Car {
+	
+	public Car()
+	{
+	}
+public void sayOdometer()
+{
+System.out.println("Your car has gone a number of miles");
+
+}
+public void sayFlat()
+{
+	System.out.println("Your car has a flat tire!");
+}
+public void sayThirsty()
+{
+	System.out.println("Your car is out of gas!");
+}
+}
+
---
+++ src/ObjectOrientedProgramming/CarReal.java
@@ -0,0 +1,10 @@
+package ObjectOrientedProgramming;
+
+public class CarReal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	}
+
+}
