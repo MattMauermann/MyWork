9be2b2694462e42bd1508a5675646009571ca4eb
Matthew Mauermann <mauermann5@outlook.com>
2018-10-12 09:02:45 -0500

    Homeimprovement program

---
+++ src/ObjectOrientedProgramming/homeImprovementTest.java
@@ -0,0 +1,13 @@
+package ObjectOrientedProgramming;
+
+public class homeImprovementTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+homeImprovement bob= new homeImprovement();
+bob.sayPaint(20, 20);
+bob.sayPlant(15);
+bob.sayPool(25, 25);
+	}
+
+}
