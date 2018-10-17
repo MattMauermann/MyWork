2ef836b48b28769994c2b5731125bb0ed6a454fa
Matthew Mauermann <mauermann5@outlook.com>
2018-10-17 08:45:00 -0500

    Greeter Class and greeter test

---
+++ src/ObjectOrientedProgramming/greeterTest.java
@@ -0,0 +1,12 @@
+package ObjectOrientedProgramming;
+
+public class greeterTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Greeter bob=new Greeter();
+bob.setAge(4);
+System.out.println(bob.getAge());
+	}
+
+}
