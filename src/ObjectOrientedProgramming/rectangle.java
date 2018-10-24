916b731e3bfa432b404f2b76e10d916cc8363c5b
Matthew Mauermann <mauermann5@outlook.com>
2018-10-24 09:01:30 -0500

    Rectangle changes

--- src/ObjectOrientedProgramming/rectangle.java
+++ src/ObjectOrientedProgramming/rectangle.java
@@ -1,34 +1,50 @@
 package ObjectOrientedProgramming;
 
 public class rectangle {
-public double length;
-public double width;
-public rectangle() {
-	length=-999;
-	width=-999;
-}
-public rectangle(double newlength,double newwidth) {
-	length=newlength;
-	width=newwidth;
-}
-public double getlength() {
-	return length;
-}
-public double getwidth() {
-	return width;
-}
-public double perimeter() {
-	double perimeter=(width+length)*2;
-	return perimeter;
-}
-public double area() {
-	double area=width*length;
-	return area;
-}
-public void setlength(double xlength) {
-	length=xlength;
-}
-public void setwidth(double xwidth) {
-	width=xwidth;
-}
+	public double length;
+	public double width;
+
+	public rectangle() {
+		length = -999;
+		width = -999;
+	}
+
+	public rectangle(double newlength, double newwidth) {
+		length = newlength;
+		width = newwidth;
+	}
+
+	public double getlength() {
+		return length;
+	}
+
+	public double getwidth() {
+		return width;
+	}
+
+	public double perimeter() {
+		double perimeter = (width + length) * 2;
+		return perimeter;
+	}
+
+	public double area() {
+		double area = width * length;
+		return area;
+	}
+
+	public void setlength(double xlength) {
+		length = xlength;
+	}
+
+	public void setwidth(double xwidth) {
+		width = xwidth;
+	}
+
+	public void changelength(double changel) {
+		length = changel * length;
+	}
+
+	public void changewidth(double changew) {
+		width = changew * width;
+	}
 }
