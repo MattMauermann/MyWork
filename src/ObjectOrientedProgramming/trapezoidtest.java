7d8e9c40156e2c88376a22095c0064c203a3165c
Matthew Mauermann <mauermann5@outlook.com>
2018-10-24 09:00:30 -0500

    Trapezoid class

---
+++ src/ObjectOrientedProgramming/trapezoidtest.java
@@ -0,0 +1,16 @@
+package ObjectOrientedProgramming;
+
+import java.util.Scanner;
+
+public class trapezoidtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		trapezoid bob = new trapezoid(11, 19, 3, 5);
+		Scanner input = new Scanner(System.in);
+
+		bob.area();
+		bob.perimeter();
+	}
+
+}
