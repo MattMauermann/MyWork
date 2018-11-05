a7f57193671930867cd6c325d74a10920f1e98be
Matthew Mauermann <mauermann5@outlook.com>
2018-11-05 08:15:49 -0600

    Soda can

--- src/DecisionStructures/sodacan.java
+++ src/DecisionStructures/sodacan.java
@@ -2,4 +2,10 @@ package DecisionStructures;
 
 public class sodacan {
 
+	private int radius;
+	private int height;
+	final private double cost = 2.6;
+	private double surfacearea;
+	private double volume;
+
 }
