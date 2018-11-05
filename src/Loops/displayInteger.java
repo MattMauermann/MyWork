1ded848fb7e411fa858d63698f954c2723d91d98
Matthew Mauermann <mauermann5@outlook.com>
2018-11-05 08:32:03 -0600

    display0-100

---
+++ src/Loops/displayInteger.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class displayInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int count = 0;
+		while (count < 100) {
+			System.out.println(count);
+			count++;
+		}
+	}
+
+}
