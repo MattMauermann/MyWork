6da9f5e0b8b1cfe346462933f1ffb3df745c41aa
Matthew Mauermann <mauermann5@outlook.com>
2018-11-13 08:58:50 -0600

    display the number of hellos input

---
+++ src/Loops/forloopsnumberhellos.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class forloopsnumberhellos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int counter = 0;
+		for (counter = 1; counter <= 10; counter++) {
+			System.out.println(counter + " Hello");
+		}
+	}
+
+}
